ebbbc8472b967006791d9f304e5833e91ce6e332
Winston Ta <winstonnta@gmail.com>
2025-02-25 10:42:38 -0800

    main.py top comments

--- Lab 5/main.py
+++ Lab 5/main.py
@@ -1,1 +1,7 @@
-# test initialization of main.py for lab 5
+'''
+Name(s): Winston Ta, Mindy Yun
+Date: 2025/02/27
+Description: This project allows users to move a rectangle across a 20x20 grid. Using console inputs, the rectangle will
+move in the corresponding direction of the user's choice.
+'''
+
