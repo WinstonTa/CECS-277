e15ec2362b87aad3b445bc8e73b7757e38c5a51c
Winston Ta <winstonnta@gmail.com>
2025-02-25 11:09:40 -0800

    rectangle class complete implementation

--- Lab 5/rectangle.py
+++ Lab 5/rectangle.py
@@ -7,20 +7,26 @@ class Rectangle:
         self.w = w
         self.h = h
 
-    def get_coords(self):
+    def get_coords(self) -> list:
         """returns the x and y values as a pair (either a tuple or a list)"""
+        return [self.x, self.y]
     
-    def get_dimensions(self):
+    def get_dimensions(self) -> list:
         """returns the rectangle’s width and height as a pair."""
+        return [self.w, self.h]
     
     def move_up(self):
         """moves the rectangle up one row"""
+        self.y -= 1
     
     def move_down(self):
         """moves the rectangle down one row"""
+        self.y += 1
     
     def move_left(self):
         """moves the rectangle left one column"""
+        self.x -= 1
 
     def move_right(self):
         """moves the rectangle right one column"""
+        self.x += 1
