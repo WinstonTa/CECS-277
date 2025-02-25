8314b28c7e7fee7354725ad1773831af47b71aff
Mindy Yun <myyun48@gmail.com>
2025-02-25 11:40:15 -0800

    11:40

--- Lab 5/main.py
+++ Lab 5/main.py
@@ -18,8 +18,7 @@ def reset_grid(grid):
     """pass in the grid and overwrite the contents with all ‘.’s"""
     for row in grid:
         for item in row:
-            print(".", end = " ")
-        print()
+            grid[row][item] = '.'
 
 def place_rect(grid, rect):
     """pass in the grid and the rectangle. At the location of
@@ -59,24 +58,28 @@ def main():
         # user chooses to move rectangle up
         if user_choice == 1:
             our_rectangle.move_up()
+            reset_grid(gridspace)
             place_rect(gridspace, our_rectangle)
             display_grid(gridspace)  
 
         # user chooses to move rectangle up  
         elif user_choice == 2:
             our_rectangle.move_down()
+            reset_grid(gridspace)
             place_rect(gridspace, our_rectangle)
             display_grid(gridspace)
 
         # user chooses to move rectangle up
         elif user_choice == 3:
             our_rectangle.move_left()
+            reset_grid(gridspace)
             place_rect(gridspace, our_rectangle)
             display_grid(gridspace)
 
         # user chooses to move rectangle up
         elif user_choice == 4:
             our_rectangle.move_right()
+            reset_grid(gridspace)
             place_rect(gridspace, our_rectangle)
             display_grid(gridspace)
 
