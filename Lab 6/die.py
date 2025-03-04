10cdcba996d109f52933121706048761ef0700a5
Winston Ta <winstonnta@gmail.com>
2025-03-04 11:00:21 -0800

    updated die.py and README

--- Lab 6/die.py
+++ Lab 6/die.py
@@ -1,18 +1,24 @@
+import random
+
 class Die:
-    def __initit__(self, sides = 6):
-        pass
+    def __initit__(self, sides: int = 6):
+        self._sides = sides
+        self._value = 0
 
-    def roll(self):
-        pass
+    def roll(self) -> int:
+        self._value = random.randint(1, self._sides)
+        return self._value
 
-    def __str__(self):
-        pass
+    def __str__(self) -> str:
+        return str(self._value)
 
-    def __lt__(self, other):
-        pass
+    def __lt__(self, other) -> bool:
+        if self._value < other:
+            return True
 
-    def __eq__(self, other):
-        pass
+    def __eq__(self, other) -> bool:
+        if self._value == other:
+            return True
 
-    def __sub__(self, other):
-        pass
+    def __sub__(self, other) -> int:
+        return self._value - other
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ CECS 277 homework and projects
 ## Lab 4 - State Capitals Quiz
 
 ## Lab 5 - Moving Rectangle
+
+## Lab 6 - Yahtzee
