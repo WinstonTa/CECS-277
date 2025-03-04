806c652474ddb4084ed8efa9f5c020aed1234b56
Winston Ta <winstonnta@gmail.com>
2025-03-04 11:33:46 -0800

    take_turn() and main() completed 1st iteration

--- Lab 6/main.py
+++ Lab 6/main.py
@@ -18,13 +18,38 @@ def take_turn(player: player):
     player.roll_dice()
     print(player.__str__())
 
+    # checks for any win types and prints congratulatory message
+    if player.has_pair():
+        print("You got a pair!")
+    elif player.has_three_of_a_kind():
+        print("You got 3 of a kind!")
+    elif player.has_series():
+        print("You got a series of 3!")
+    else:
+        print("Aww. Too Bad.")
+    
+    print(f"Score = {player.points()}")
+
+
 def main():
     '''
     construct a player object
     repeatedly call take_turn() until user chooses to end game
     display final points at the end of the game
     '''
-    pass
+    # initialize player and play again status
+    player = player()
+    play_again = True
+
+    print("-Yahtzee-\n")
+    while True:
+        take_turn(player)
+        play_again = check_input.get_yes_no()
+
+        # end the game if the user decides to stop playing
+        if not play_again:
+            break
+
 
 if __name__ == '__main__':
     main()
