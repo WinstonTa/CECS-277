150d133902276601bfe80d23b188ceb1dcffd38b
Mindy Yun <myyun48@gmail.com>
2025-03-04 11:35:49 -0800

    11:35

--- Lab 6/die.py
+++ Lab 6/die.py
@@ -1,24 +1,31 @@
 import random
 
 class Die:
+    '''passes in the number of sides of the die. Assign sides
+        using the parameter and set value to either 0 or to the returned value of roll()'''
     def __initit__(self, sides: int = 6):
         self._sides = sides
         self._value = 0
 
+    '''generate a random number between 1 and the number of sides and assign it to value and return it'''
     def roll(self) -> int:
         self._value = random.randint(1, self._sides)
         return self._value
 
+    '''return the Die's value as a string'''
     def __str__(self) -> str:
         return str(self._value)
 
+    '''return true if the value of self is less than the value of the other'''
     def __lt__(self, other) -> bool:
         if self._value < other:
             return True
 
+    '''return true if the value of self is equal to the value of other'''
     def __eq__(self, other) -> bool:
         if self._value == other:
             return True
 
+    '''return the difference ebtween the value of self and the value of other'''
     def __sub__(self, other) -> int:
         return self._value - other
--- Lab 6/player.py
+++ Lab 6/player.py
@@ -1,13 +1,17 @@
 import die
 
 class Player():
+    '''constructs and sorts the list of three Die objects and then initializes
+        the player’s points to 0'''
     def __init__(self):
         self._dice = [die.Die(), die.Die(), die.Die()]
         self._points = 0
 
+    '''get property that returns the player's points'''
     def points(self):
         return self._points
     
+    '''calls roll on each of the Die objects in the list and then sorts the list'''
     def roll_dice(self):
         die1 = self._dice[0].roll()
         die2 = self._dice[1].roll()
@@ -16,22 +20,25 @@ class Player():
         self._dice = [die1, die2, die3]
         self._dice.sort()
 
-
+    '''returns true if two dice in the list have the same value, increments points by 1 if true'''
     def has_pair(self):
         if self._dice[0] == self._dice[1] or self._dice[1] == self._dice[2] \
         or self._dice[0] == self._dice[2]:
             self.points += 1
             return True
-        
+
+    '''returns true if all three dice in the list have the same value, increments points by 3 if true'''  
     def has_three_of_a_kind(self):
         if self._dice[0] == self._dice[1] and self._dice[1] == self._dice[2]:
             self.points += 3
             return True
-        
+
+    '''returns true if the value of each dice in the list are in a sequence, increments points by 2 if true'''    
     def has_series(self):
         if (self._dice[2] - self._dice[1]) == 1 and (self._dice[1] - self._dice[0]) == 1:
             self.points += 2
             return True
 
+    '''returns a string in the format: “D1=2, D2=4, D3=6”'''
     def __str__(self):
         return f"D1={self._dice[0]} D2={self._dice[1]} D3={self._dice[2]}"
