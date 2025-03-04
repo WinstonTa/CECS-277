313507f839e64950d773a018ea755f2ae07fd01e
Winston Ta <winstonnta@gmail.com>
2025-03-04 10:42:48 -0800

    die boilerplate

---
+++ Lab 6/die.py
@@ -0,0 +1,18 @@
+class Die:
+    def __initit__(self, sides = 6):
+        pass
+
+    def roll(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
