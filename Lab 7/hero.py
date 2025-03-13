33f479e25d345f860b7742c40a0a56b73dc1ea5b
Winston Ta <winstonnta@gmail.com>
2025-03-13 11:06:42 -0700

    hero documentation

--- Lab 7/hero.py
+++ Lab 7/hero.py
@@ -3,7 +3,18 @@ import random
 
 class Hero(entity.Entity()):
 
-    def sword_attack(self, dragon):
+    def sword_attack(self, dragon: entity) -> str:
+        """
+        Hero inflicts sword attack.
+
+        Args:
+            dragon (entity): the dragon (passed in as an entity) that will be hurt through sword attack
+
+        Returns:
+            A string describing the sword attack.
+        """
+
+        # randomizes 2 dice with rolls from 1-6, then totals it, then dragon takes that total damage
         d1 = random.randint(1, 6)
         d2 = random.randint(1, 6)
         total_dmg = d1 + d2
@@ -11,7 +22,18 @@ class Hero(entity.Entity()):
 
         return f"You attacked the {dragon.name} with your sword for {total_dmg} damage."
 
-    def arrow_attack(self, dragon):
+    def arrow_attack(self, dragon: entity) -> str:
+        """
+        Hero inflicts arrow attack.
+
+        Args:
+            dragon (entity): the dragon (passed in as an entity) that will be hurt through arrow attack
+
+        Returns:
+            A string describing the arrow attack.
+        """
+
+        # randomizes 1 dice with rolls from 1-12, then dragon takes that damage
         d = random.randint(1, 12)
         dragon.take_damage(d)
 
