f1844aa4e7139dd0e9de4522b8d9568d822282bf
Mindy Yun <myyun48@gmail.com>
2025-03-13 11:10:28 -0700

    11:10

--- Lab 7/main.py
+++ Lab 7/main.py
@@ -16,8 +16,9 @@ import random
 # introduction, create hero
 winston = input('What is your name, challenger?\n')
 winston = hero.Hero(winston, 50)
-print('Welcome to dragon training, Astrid\nYou must defeat 3 dragons.')
+print(f'Welcome to dragon training, {winston}\nYou must defeat 3 dragons.')
 
+# create and name dragons
 reg_dragon = dragon.Dragon('Deadly Lizzettey', 10)
 fire_dragon = fire.Fire('Acefury', 15)
 wind_dragon = flying.Flying('Windy Mindy', 20)
@@ -26,23 +27,27 @@ dragons_list = [reg_dragon, fire_dragon, wind_dragon]
 
 print('dragons_list')
 
+# while dragons are alive
 while len(dragons_list > 0):
 
+    # menu of hero and dragons hp
     print(hero.__str__())
     print(f'1. Attack {reg_dragon}: {reg_dragon.__str__()}')
     print(f'2. Attack {fire_dragon}: {fire_dragon.__str__()}')
     print(f'3. Attack {wind_dragon}: {wind_dragon.__str__()}')
 
+    # chhoosing which dragon to attack with which wepon
     attack_dragon = check_input.get_int_range('Choose a dragon to attack: ', 1, 3)
     print('Attack with:\n1. Arrow (1 D12)\n2. Sword (2 D6)')
     weapon = check_input.get_int_range('Enter weapon: ', 1, 2)
 
+    # hero attacks
     if weapon == 1:
         print(winston.arrow_attack(dragons_list[attack_dragon]))
     else:
         print(winston.sword_attack(dragons_list[attack_dragon]))
 
+    # dragon attacks
     random_attack = random.randint(len(dragons_list))
     print(dragons_list(random_attack).special_attack())
 
-    if reg_dragon.
