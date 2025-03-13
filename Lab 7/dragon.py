58183a5cd48969a830078c8fecc76c92fe833f0b
Winston Ta <winstonnta@gmail.com>
2025-03-13 10:19:39 -0700

    finish dragon.py

--- Lab 7/dragon.py
+++ Lab 7/dragon.py
@@ -1,9 +1,16 @@
 import entity
+import random
 
 class Dragon(entity.Entity()):
     
     def basic_attack(self, hero):
-        pass
+        d = random.randint(2, 5)
+        hero.take_damage(d)
+
+        return f"{self.name} smashes you with their tail for {d} damage!"
 
     def special_attack(self, hero):
-        pass
+        d = random.randint(3, 7)
+        hero.take_damage(d)
+
+        return f"{self.name} slashes you with their claws for {d} damage!"
