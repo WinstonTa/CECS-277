f782b20d71fb6078108c823a4550120e2d4d113e
Winston Ta <winstonnta@gmail.com>
2025-03-13 10:37:56 -0700

    flying.py finished

--- Lab 7/flying.py
+++ Lab 7/flying.py
@@ -1,4 +1,5 @@
 import dragon
+import random
 
 class FlyingDragon(dragon.Dragon()):
 
@@ -7,7 +8,14 @@ class FlyingDragon(dragon.Dragon()):
         self._swoops = 3
 
     def special_attack(self, hero):
-        pass
+        d = random.randint(5, 8)
+        if self._swoops > 0:
+            self._swoops -= 1
+            hero.take_damage(d)
+
+            return f"{self.name} swoops you with their majestic grace for {d} damage!"
+        else:
+            return f"{self.name} tried to fly up and majestically destroy you, but ran out of energy before attacking!"
 
     def __str__(self):
-        pass
+        super().__str__() + f"\nSwoop Attacks Remaining: {self._swoops}"
