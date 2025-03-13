cb004d31c2af0016420ab767db83c0d6172f54f8
Winston Ta <winstonnta@gmail.com>
2025-03-13 10:49:42 -0700

    dragon documentation

--- Lab 7/dragon.py
+++ Lab 7/dragon.py
@@ -3,13 +3,33 @@ import random
 
 class Dragon(entity.Entity()):
     
-    def basic_attack(self, hero):
+    def basic_attack(self, hero: entity) -> str:
+        """
+        Dragon inflicts basic tail attack.
+
+        Args:
+            hero (entity): the hero (passed in as an entity) that will be hurt through tail attack
+        
+        Returns:
+            A string describing the tail attack.
+        """
+        # tail attack does random damage from 2-5 hp
         d = random.randint(2, 5)
         hero.take_damage(d)
 
         return f"{self.name} smashes you with their tail for {d} damage!"
 
-    def special_attack(self, hero):
+    def special_attack(self, hero: entity) -> str:
+        """
+        Dragon inflicts special claw attack.
+
+        Args:
+            hero (entity): the hero (passed in as an entity) that will be hurt through claw attack
+        
+        Returns:
+            A string describing the claw attack.
+        """
+        # claw attack does random damage from 3-7 hp
         d = random.randint(3, 7)
         hero.take_damage(d)
 
