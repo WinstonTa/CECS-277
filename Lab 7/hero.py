2f85580b5dcae219ef220af935c9aaef88ac78f8
Winston Ta <winstonnta@gmail.com>
2025-03-13 10:15:01 -0700

    complete implementation of hero.py

--- Lab 7/hero.py
+++ Lab 7/hero.py
@@ -1,9 +1,18 @@
 import entity
+import random
 
 class Hero(entity.Entity()):
 
     def sword_attack(self, dragon):
-        pass
+        d1 = random.randint(1, 6)
+        d2 = random.randint(1, 6)
+        total_dmg = d1 + d2
+        dragon.take_damage(total_dmg)
+
+        return f"You attacked the {dragon.name} with your sword for {total_dmg} damage."
 
     def arrow_attack(self, dragon):
-        pass
+        d = random.randint(1, 12)
+        dragon.take_damage(d)
+
+        return f"You attacked the {dragon.name} with your bow for {d} damage."
