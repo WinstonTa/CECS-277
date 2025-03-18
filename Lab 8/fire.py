61949b676d37c589c28d0944de5aa93e206e2131
Mindy Yun <myyun48@gmail.com>
2025-03-18 11:18:49 -0700

    11:18

--- Lab 8/fire.py
+++ Lab 8/fire.py
@@ -11,50 +11,57 @@ class Fire(pokemon.Pokemon):
             n = random.randint(0,2)
             self._name = fire_names[n]
 
+    '''Override get_special_menu'''
+    # display fire attacks
+    def special_menu(self):
+        print("1. Ember\n2. Fire Blast")
+
+    # ember special attack
     def _ember(self, opponent):
         damage = random.randint(2,6)
 
         # fire vs fire: not very effective
         if opponent._type == 0:
             damage *= 0.5
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} BURNS {opponent._name} with EMBERS for {damage} damage. \
                 It was not very effective.'
         
         # fire vs water: not very effective
         elif opponent._type == 1:
             damage *= 0.5
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} BURNS {opponent._name} with EMBERS for {damage} damage. \
                 It was not very effective.'
     
         # fire vs grass: super effective
         else:
             damage *= 2
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} BURNS {opponent._name} with EMBERS for {damage} damage. \
                 It was SUPER EFFECTIVE!'
 
+    # fire blast special attack
     def _fire_blast(self, opponent):
         damage = random.randint(1,7)
 
         # fire vs fire: not very effective
         if opponent._type == 0:
             damage *= 0.5
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} uses FIRE BLAST against {opponent._name} for {damage} damage. \
                 It was not very effective.'
         
         # fire vs water: not very effective
         elif opponent._type == 1:
             damage *= 0.5
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} uses FIRE BLAST against {opponent._name} for {damage} damage. \
                 It was not very effective.'
     
         # fire vs grass: super effective
         else:
             damage *= 2
-            opponent._hp -= damage
+            opponent._take_damage(damage)
             return f'{self._name} uses FIRE BLAST against {opponent._name} for {damage} damage. \
                 It was SUPER EFFECTIVE!'
