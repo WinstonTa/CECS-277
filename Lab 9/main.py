d59174f2030f9927c04fa29e506047cc85cf1ec3
Mindy Yun <myyun48@gmail.com>
2025-04-10 12:08:43 -0700

    11:08

--- Lab 9/main.py
+++ Lab 9/main.py
@@ -8,8 +8,14 @@ def main():
     m = maze.Maze()
     p = player.Player()
     g = ghost.Ghost()
+    
+    game = True
 
-    print(m._m)
+    while game == True:
+        print(m)
+        player_move = input("Move (WASD): ").lower()
+        p.move(player_move)
+        g.move()
     
 
 if __name__ == "__main__":
