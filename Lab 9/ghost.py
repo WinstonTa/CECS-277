2ee1e3da9a5559ec430679f327c1dcd2ef948020
Winston Ta <winstonnta@gmail.com>
2025-04-10 09:52:11 -0700

    ghost building blocks

--- Lab 9/ghost.py
+++ Lab 9/ghost.py
@@ -1,6 +1,22 @@
+import random
+
 class Ghost:
     def __init__(self):
         self._previous = '.'
     
-    def move(self):
-        pass
+    def move(self) -> bool:
+        # direction ghost should move next, collision variable
+        x_move = 0
+        y_move = 0
+
+        # initialize maze, find player and ghost coordinates
+        maze = maze()
+        player_location = maze.search_maze("P")
+        ghost_location = maze.search_maze("G")
+
+        if player_location[0] < ghost_location[0]:
+            x_move = 1
+        elif player_location[0] > ghost_location[0]:
+            x_move = -1
+        else:
+            pass
