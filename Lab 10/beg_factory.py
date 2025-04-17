f782c8253d733cd8c8b8903f6d23e6b40f849669
Mindy Yun <myyun48@gmail.com>
2025-04-16 21:04:38 -0700

    monster factories

--- Lab 10/beg_factory.py
+++ Lab 10/beg_factory.py
@@ -1,7 +1,14 @@
 import enemy_factory
+import beg_goblin
+import beg_troll
+import random
 
 class BeginnerEnemyFactory(enemy_factory.EnemyFactory):
-    # work on this after BegGoblin and BegTroll are done
     def create_random_enemy(self):
-        pass
+        rand_choice = random.randint(0,1)
+        if rand_choice == 0:
+            random_enemy = beg_goblin.BegGoblin()
+        else:
+            random_enemy = beg_troll.BegTroll()
+        return random_enemy
 
--- Lab 10/exp_factory.py
+++ Lab 10/exp_factory.py
@@ -1,8 +1,16 @@
 import enemy_factory
+import exp_goblin
+import exp_troll
+import random
 
 class ExpertEnemyFactory(enemy_factory.EnemyFactory):
     # work on this after ExpGoblin and ExpTroll are done
     def create_random_enemy(self):
-        pass
+        rand_choice = random.randint(0,1)
+        if rand_choice == 0:
+            random_enemy = exp_goblin.ExpGoblin("Grotesque Goblin")
+        else:
+            random_enemy = exp_troll.ExpTroll("Tyrannical Troll")
+        return random_enemy
 
     
