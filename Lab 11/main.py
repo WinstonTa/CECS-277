8c6a3be10acb0931c2c5b7c9d9f25929102209b2
Winston Ta <winstonnta@gmail.com>
2025-04-22 12:06:48 -0700

    magic item list

--- Lab 11/main.py
+++ Lab 11/main.py
@@ -6,6 +6,7 @@ a bard, warrior, or wizard, they can use 2 items from a shield, ring, or cloak t
 '''
 
 # file imports
+import check_input
 import bard
 import character
 import cloak
@@ -17,7 +18,20 @@ import wizard
 
 # main game functionality
 def main():
-    pass
+    print("Character Maker!")
+
+    character_choice = check_input.get_int_range("Choose a starting character:\n1. Bard\n2. Warrior\n3. Wizard\n", 1, 3)
+
+    magic_items = ["Sturdy Shield", "Magic Ring", "Protective Cloak"]
+    items_left = 2
+
+    while items_left > 0:
+        for i, magic_item in enumerate(magic_items):
+            print(f"Choose {items_left} items:")
+            print(f"{i + 1}. {magic_item}")
+        decoration_choices = check_input.get_int_range("", 1, len(magic_items) - 1)
+        items_left -= 1
+        
 
 # execute main game
 if __name__ == "__main__":
