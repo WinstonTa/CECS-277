8e2502d016cbaf6b90a6dc0be90c0418ffe31141
Winston Ta <winstonnta@gmail.com>
2025-04-22 11:36:08 -0700

    main lab 11 boilerplate

--- Lab 11/main.py
+++ Lab 11/main.py
@@ -5,3 +5,20 @@ Description: This project lets users build a character that will fight three dif
 a bard, warrior, or wizard, they can use 2 items from a shield, ring, or cloak to defeat 2 of 3 minimum trials to win.
 '''
 
+# file imports
+import bard
+import character
+import cloak
+import our_decorator
+import ring
+import shield
+import warrior
+import wizard
+
+# main game functionality
+def main():
+    pass
+
+# execute main game
+if __name__ == "__main__":
+    main()
