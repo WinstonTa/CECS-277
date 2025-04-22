1cc282ea73164f9797778cce5488208f1baf289e
Winston Ta <winstonnta@gmail.com>
2025-04-22 11:18:43 -0700

    character abstract methods

--- Lab 11/character.py
+++ Lab 11/character.py
@@ -1,18 +1,22 @@
 import abc
 
 class Character(abc.ABC):
+    @abc.abstractmethod
     def description(self):
         # character's name and items
         pass
 
+    @abc.abstractmethod
     def magic_resistance(self):
         # character's magic resist value
         pass
 
+    @abc.abstractmethod
     def strength(self):
         # character's strength valu
         pass
 
+    @abc.abstractmethod
     def __str__(self):
         # returns character's description, magic resistance, and strength
         pass
