05736de0e1170e40bd2c2bf4bef3681467f39e3e
Mindy Yun <myyun48@gmail.com>
2025-04-29 11:30:48 -0700

    tasklist barebones

---
+++ Lab 12/tasklist.py
@@ -0,0 +1,31 @@
+import task
+
+class TaskList():
+    def __init__(self):
+        self._tasklist = []
+        self._n = 0 # is this supposed to be 0?
+
+        # construct task object
+
+    def add_task(self, desc, date, time):
+        pass
+
+    def get_current_task(self):
+        pass
+
+    def mark_complete(self):
+        pass
+
+    def save_file(self):
+        pass
+    
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    
