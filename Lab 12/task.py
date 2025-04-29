488de1b65121e366b1c7bc00e80d1c60434b9cb2
Winston Ta <winstonnta@gmail.com>
2025-04-29 11:30:58 -0700

    epic if return lizzette aura trick of doom

--- Lab 12/task.py
+++ Lab 12/task.py
@@ -9,24 +9,18 @@ class Task():
     
     def __repr__(self): #come back to this
         pass
-    def __lt__(self, other):    
+    def __lt__(self, other):
         # compare by year
-        if int(self._date[-4:]) < int(other._date[-4:]):
-            return True
+        if int(self._date[-4:]) < int(other._date[-4:]): return True
         # compare by month
-        if int(self._date[:2]) < int(other._date[:2]):
-            return True
+        if int(self._date[:2]) < int(other._date[:2]): return True
         # compare by day
-        if int(self._date[3:5]) < int(other._date[3:5]):
-            return True
+        if int(self._date[3:5]) < int(other._date[3:5]): return True
         # compare by hour
-        if int(self._time[:2]) < int(other._time[:2]):
-            return True
+        if int(self._time[:2]) < int(other._time[:2]): return True
         # compare by minute
-        if int(self._time[-2:]) < int(other._time[-2:]):
-            return True
+        if int(self._time[-2:]) < int(other._time[-2:]): return True
         # compare by alphabetical order
-        if self._desc < other._desc:
-            return True
+        if self._desc < other._desc: return True
         return False
         
