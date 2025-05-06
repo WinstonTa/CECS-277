342fe3eea0cb6b2305ccf4946315bda9c2324bcf
Winston Ta <winstonnta@gmail.com>
2025-05-06 11:42:00 -0700

    state asleep docstrings

--- Lab 13/state_asleep.py
+++ Lab 13/state_asleep.py
@@ -2,11 +2,29 @@ import puppy_state
 import state_eat
 
 class State_Asleep(puppy_state.Puppy_State):
-    def feed(self, puppy):
+    def feed(self, puppy) -> str:
+        """
+        Puppy gets fed.
+
+        Args:
+            puppy: Puppy gets fed from the state of being asleep.
+
+        Returns:
+            A string detailing the feeding session.
+        """
         puppy.change_state(state_eat.State_Eat())
         puppy.inc_feeds()
         return "The puppy wakes up and comes running to eat."
 
     def play(self, puppy):
+        """
+        Puppy attempts to play.
+
+        Args:
+            puppy: Puppy gets fed from the state of being asleep.
+
+        Returns:
+            A string detailing the failed play session.
+        """
         puppy.reset()
         return "The puppy is asleep. It doesn't want to play right now."
