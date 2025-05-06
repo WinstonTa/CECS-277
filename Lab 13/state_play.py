68b36bef038b91d597a7ed0c582e2ca15d26ea93
Winston Ta <winstonnta@gmail.com>
2025-05-06 11:43:53 -0700

    state play docstrings

--- Lab 13/state_play.py
+++ Lab 13/state_play.py
@@ -3,13 +3,32 @@ import state_asleep
 
 class State_Play(puppy_state.Puppy_State):
     def feed(self, puppy):
+        """
+        Puppy attempts to be fed.
+
+        Args:
+            puppy: Puppy tries to get fed from a state of play.
+
+        Returns:
+            A string detailing the failed feeding session.
+        """
         return "The puppy is too busy playing with the ball to eat right now."
 
     def play(self, puppy):
+        """
+        Puppy plays from being played.
+
+        Args:
+            puppy: Puppy play count increments.
+
+        Returns:
+            A string detailing the various play sessions, depending on how exhausted the puppy is.
+        """
+        # puppy kind of tired
         if puppy._plays == 1:
             puppy.inc_plays()
             return "You throw the ball again and the puppy excitedly chases it."
-        elif puppy._plays == 2:
+        elif puppy._plays == 2:     # puppy super tired
             puppy.change_state(state_asleep.State_Asleep())
             puppy.reset()
             return "You throw the ball again and the puppy excitedly chases it.\
