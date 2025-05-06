668593833de463713db9c6970cbb2dfbe8d19078
Winston Ta <winstonnta@gmail.com>
2025-05-06 11:44:22 -0700

    puppy state docstring

--- Lab 13/puppy_state.py
+++ Lab 13/puppy_state.py
@@ -3,8 +3,10 @@ import abc
 class Puppy_State(abc.ABC):
     @abc.abstractmethod
     def feed(self, puppy):
+        # abstract feed method
         pass
 
     @abc.abstractmethod
     def play(self, puppy):
+        # abstract play method
         pass
