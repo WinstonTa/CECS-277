d6f7d9b297edaffd5cd9dcc103ee500d8834dc41
Mindy Yun <myyun48@gmail.com>
2025-05-06 10:55:29 -0700

    puppy mostly done?

---
+++ Lab 13/puppy.py
@@ -0,0 +1,27 @@
+import state_asleep
+import puppy_state
+
+class Puppy():
+    def __init__(self):
+        self._state = state_asleep.StateAsleep()
+        self._feeds = 0
+        self._plays = 0
+
+    def change_state(self, new_state):
+        self._state = new_state
+
+    def throw_ball(self):
+        return self._state.play(self)
+
+    def give_food(self):
+        return self._state.feed(self)
+
+    def inc_feeds(self):
+        self._feeds += 1
+
+    def inc_plays(self):
+        self._plays += 1
+
+    def reset(self):
+        self._feeds = 0
+        self._plays = 0
