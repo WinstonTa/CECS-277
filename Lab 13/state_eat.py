578444a63d60dabbbc096034ca6602a9e1a016d0
Mindy Yun <myyun48@gmail.com>
2025-05-06 11:47:17 -0700

    state eat docstring done

--- Lab 13/state_eat.py
+++ Lab 13/state_eat.py
@@ -4,6 +4,15 @@ import state_play
 
 class State_Eat(puppy_state.Puppy_State):
     def feed(self, puppy):
+        """
+        Puppy eats.
+
+        Args:
+            puppy : Puppy gets fed or eats so much that it falls asleep.
+
+        Returns:
+            A string detailing the eating process.
+        """
         if puppy._feeds == 1:
             puppy.inc_feeds()
             return "The puppy continues to eat as you add another scoop of kibble to its bowl."
@@ -14,6 +23,15 @@ class State_Eat(puppy_state.Puppy_State):
                     \nThe puppy ate so much it fell asleep!"
 
     def play(self, puppy):
+        """
+        Puppy plays.
+
+        Args:
+            puppy : Puppy plays from a state of eating.
+
+        Returns:
+            A string detailing the play session.
+        """
         puppy.change_state(state_play.State_Play())
         puppy.inc_plays()
         return "The puppy looks up from its food and chases the ball you threw."
